94f613d38109bd064c95c1e5c2023d8e4d092b9e
TheRhino6 <ryanandrewsmedia@gmail.com>
2026-08-02 09:24:13 +0100

    file checker

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,6 +8,13 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
         if valid_target_dir == False:
             raise ValueError(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        print (f"Target directory: {target_dir}")
+        for item in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, item)
+            if os.path.isfile(item_path):
+                print(f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir=False")
+            elif os.path.isdir(item_path):
+                print(f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir=True")
         if not os.path.isdir(target_dir):
             raise ValueError(f'Error: "{directory}" is not a directory')
         return f"Success: \"{directory}\" is within the working directory"
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -5,9 +5,9 @@ from functions.get_files_info import get_files_info
 
 def test ():
     print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "pkg"))
     print(get_files_info("calculator", "/bin"))
     print(get_files_info("calculator", "../"))
-    print(get_files_info("calculator", "main.py"))
 
 if __name__ == "__main__":
     test()

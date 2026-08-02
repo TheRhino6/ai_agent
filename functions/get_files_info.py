8514b9457ae9f5e01642fd9c1162d14431efa370
TheRhino6 <ryanandrewsmedia@gmail.com>
2026-08-02 13:11:10 +0100

    edit to get files info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,7 +8,6 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
         if valid_target_dir == False:
             raise ValueError(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-        print (f"Target directory: {target_dir}")
         for item in os.listdir(target_dir):
             item_path = os.path.join(target_dir, item)
             if os.path.isfile(item_path):
